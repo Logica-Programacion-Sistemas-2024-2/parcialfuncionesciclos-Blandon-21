b1132295f648c4ce0bdc322612c83f598f2429b0
Blandon-21 <tomyblandon21@gmail.com>
2024-08-23 13:40:36 -0500

    solucion parcial

--- app/src/main/java/parcial1g2/App.java
+++ app/src/main/java/parcial1g2/App.java
@@ -1,12 +1,38 @@
-
 package parcial1g2;
 
+import java.util.Scanner;
 
 public class App {
     
     public static void main(String[] args) {
         
         // Logica de solución
+        Scanner sc = new Scanner(System.in);
+
+        int cantidadVehiculos = 0;
+        int modeloVehiculos = 0;
+        int avaluoVehiculos = 0;
+        int impuestosRodamiento = 0;
+        int tasadeseguro = 0;
+        int tasaderodamiento = 0;
+
+        do{
+            System.out.println("Cuantos vehiculos desea calcular: ");
+
+            cantidadVehiculos = sc.nextInt();
+
+            System.out.println("Ingrese la decada del modelo del vehiculo?\n----Modelo-----\n1)90  \n2)00 \n3)10  \n4)20 ");
+            
+            modeloVehiculos = sc.nextInt();
+
+            System.out.println("Ingrese el avaluo del vehiculo");
+            
+            avaluoVehiculos = sc.nextInt();
+        }
+    
+
+        
+
 
     }
 
@@ -20,7 +46,28 @@ public class App {
     */
     //------------------------------------------------------------------
     
+public static int cantidadVehiculos (int impuestosRodamiento, int avaluoVehiculos){
 
+    try{
+        
+        int impuestosRodamiento = 0;
+
+            impuestosRodamiento = avaluoVehiculos * 4.09;
+
+            return impuestosRodamiento;
+
+        } catch (Exception e){
+            return -1;
+        }
+    }
+    switch (impuestosRodamiento) {
+        case 1:
+
+            impuestosRodamiento = avaluoVehiculos * tasaderodamiento;
+            
+            break
+    }
+}
 
     //------------------------------------------------------------------
 
